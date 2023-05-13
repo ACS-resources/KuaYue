5c6b6b7ca022f9a15f03f0c1b20752ff83dc9f7d
柳树怪 <wolliw@163.com>
2023-05-13 12:49:00 +0800

    Revert "将0.1.1版本移植到1.19.2"
    This reverts commit 05d96b1e2febad1c0c94c65140ebd20b0beb0872.

--- src/main/java/willow/train/kuayue/Items/ToolTipsItemHelper.java
+++ src/main/java/willow/train/kuayue/Items/ToolTipsItemHelper.java
@@ -1,6 +1,7 @@
 package willow.train.kuayue.Items;
 
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
@@ -23,9 +24,9 @@ public class ToolTipsItemHelper extends BlockItem {
 
     @Override
     public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltip, TooltipFlag pFlag) {
-        Component translatableComponent = Component.translatable((pStack.getDescriptionId()+".tooltip"));
-        Component translatableComponent2 =  Component.translatable((pStack.getDescriptionId()+".tooltip2"));
-        Component translatableComponent3 =  Component.translatable((pStack.getDescriptionId()+".tooltip3"));
+        TranslatableComponent translatableComponent = new TranslatableComponent(pStack.getDescriptionId()+".tooltip");
+        TranslatableComponent translatableComponent2 = new TranslatableComponent(pStack.getDescriptionId()+".tooltip2");
+        TranslatableComponent translatableComponent3 = new TranslatableComponent(pStack.getDescriptionId()+".tooltip3");
         if(!translatableComponent.getString().equals("")&&!translatableComponent.getString().equals(pStack.getDescriptionId()+".tooltip")) pTooltip.add(translatableComponent);
         if(!translatableComponent2.getString().equals("")&&!translatableComponent2.getString().equals(pStack.getDescriptionId()+".tooltip2")) pTooltip.add(translatableComponent2);
         if(!translatableComponent3.getString().equals("")&&!translatableComponent3.getString().equals(pStack.getDescriptionId()+".tooltip3")) pTooltip.add(translatableComponent3);
