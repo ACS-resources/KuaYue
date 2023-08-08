b0a2d2f2cc9f6d0d135f3eb72728ddd12aa18399
zlw380 <496607959@qq.com>
2023-08-08 21:12:28 +0800

    使压路机不会破坏标准轨

--- src/main/java/willow/train/kuayue/mixin/MixinRollerMovementBehaviour.java
+++ src/main/java/willow/train/kuayue/mixin/MixinRollerMovementBehaviour.java
@@ -7,19 +7,20 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.Overwrite;
 import willow.train.kuayue.init.KYCreateBlock;
 
 @Mixin(RollerMovementBehaviour.class)
-public class MixinRollerMovementBehaviour {
+public class MixinRollerMovementBehaviour extends BlockBreakingMovementBehaviour {
 
-    @Redirect(method = "canBreak",
-            at = @At(value = "INVOKE",
-                    target = "Lcom/simibubi/create/content/kinetics/base/BlockBreakingMovementBehaviour;canBreak(Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;)Z"),remap = false)
-    public boolean redirectCanBreak(BlockBreakingMovementBehaviour instance, Level world, BlockPos breakingPos, BlockState state) {
+    /**
+     * @author zlw380
+     * @reason 使压路机不会破坏标准轨
+     */
+    @Overwrite(remap = false)
+    public boolean canBreak(Level world, BlockPos breakingPos, BlockState state) {
 
-        return instance.canBreak(world, breakingPos, state) && !state.getCollisionShape(world, breakingPos)
+        return super.canBreak(world, breakingPos, state) && !state.getCollisionShape(world, breakingPos)
                 .isEmpty() && !AllBlocks.TRACK.has(state) && !KYCreateBlock.STANDARD_TRACK.has(state);
     }
 
