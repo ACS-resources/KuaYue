9aa1ba56bbc03af4e7688310e2a931ed3734e12f
zlw380 <496607959@qq.com>
2023-08-12 08:10:06 +0800

    修复了转向架在组装列车时会反方向渲染的bug

--- src/main/java/willow/train/kuayue/Blocks/Bogeys/df11g_bogey/DF11GBogeyRenderer.java
+++ src/main/java/willow/train/kuayue/Blocks/Bogeys/df11g_bogey/DF11GBogeyRenderer.java
@@ -6,7 +6,13 @@ import com.mojang.blaze3d.vertex.VertexConsumer;
 import com.simibubi.create.content.trains.bogey.BogeyRenderer;
 import com.simibubi.create.content.trains.bogey.BogeySizes;
 import com.simibubi.create.content.trains.entity.CarriageBogey;
+import com.simibubi.create.foundation.utility.Couple;
+import com.simibubi.create.foundation.utility.NBTHelper;
+import com.simibubi.create.foundation.utility.animation.LerpedFloat;
+import net.minecraft.core.Direction;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.phys.Vec3;
+import willow.train.kuayue.base.Constants;
 import willow.train.kuayue.init.KYBogeySizes;
 
 import static willow.train.kuayue.init.KYBlockPartials.*;
@@ -37,22 +43,63 @@ public class DF11GBogeyRenderer {
         @Override
         public void render(CompoundTag bogeyData, float wheelAngle, PoseStack ms, int light, VertexConsumer vb, boolean inContraption) {
 
+            /*if (yaw != null){
+                double tickYaw = yaw.getValue();
+                //System.out.println("tickYaw:"+tickYaw);
+            }*/
+
+            boolean forwards = DF11GBogeyRenderer.isForwards(bogeyData, inContraption);
+
+            Direction direction = bogeyData.contains(Constants.BOGEY_ASSEMBLY_DIRECTION_KEY)
+                    ? NBTHelper.readEnum(bogeyData, Constants.BOGEY_ASSEMBLY_DIRECTION_KEY, Direction.class)
+                    : Direction.NORTH;
+
             boolean inInstancedContraption = vb == null;
 
             BogeyModelData frame = getTransform(DF11G_FRAME, ms, inInstancedContraption);
-            frame.translate(0,0.375,0).render(ms, light, vb);
-
             BogeyModelData[] wheels = getTransform(DF11G_WHEEL, ms, inInstancedContraption, 3);
 
-            for (int side = -1; side < 2; side++) {
-                if (!inInstancedContraption)
-                    ms.pushPose();
-                BogeyModelData wheel = wheels[side + 1];
-                wheel.translate(0, 0.88, ((double)side) * 2d)
-                        .rotateX(wheelAngle)
-                        .render(ms, light, vb);
-                if (!inInstancedContraption)
-                    ms.popPose();
+            if (direction == Direction.NORTH || direction == Direction.WEST){
+                if (forwards){
+                    frame.translate(0,0.375,0).render(ms, light, vb);
+
+                    for (int side = -1; side < 2; side++) {
+                        if (!inInstancedContraption)
+                            ms.pushPose();
+                        BogeyModelData wheel = wheels[side + 1];
+                        wheel.translate(0, 0.88, ((double)side) * 2d)
+                                .rotateX(wheelAngle)
+                                .render(ms, light, vb);
+                        if (!inInstancedContraption)
+                            ms.popPose();
+                    }
+                }else {
+                    frame.rotateY(180).translate(0,0.375,0).render(ms, light, vb);
+
+                    for (int side = -1; side < 2; side++) {
+                        if (!inInstancedContraption)
+                            ms.pushPose();
+                        BogeyModelData wheel = wheels[side + 1];
+                        wheel.translate(0, 0.88, ((double)side) * 2d)
+                                .rotateX(wheelAngle)
+                                .render(ms, light, vb);
+                        if (!inInstancedContraption)
+                            ms.popPose();
+                    }
+                }
+            }else {
+                frame.translate(0,0.375,0).render(ms, light, vb);
+
+                for (int side = -1; side < 2; side++) {
+                    if (!inInstancedContraption)
+                        ms.pushPose();
+                    BogeyModelData wheel = wheels[side + 1];
+                    wheel.translate(0, 0.88, ((double)side) * 2d)
+                            .rotateX(wheelAngle)
+                            .render(ms, light, vb);
+                    if (!inInstancedContraption)
+                        ms.popPose();
+                }
             }
         }
     }
@@ -63,23 +110,59 @@ public class DF11GBogeyRenderer {
         @Override
         public void render(CompoundTag bogeyData, float wheelAngle, PoseStack ms, int light, VertexConsumer vb, boolean inContraption) {
 
+            boolean forwards = DF11GBogeyRenderer.isForwards(bogeyData, inContraption);
+
+            Direction direction = bogeyData.contains(Constants.BOGEY_ASSEMBLY_DIRECTION_KEY)
+                    ? NBTHelper.readEnum(bogeyData, Constants.BOGEY_ASSEMBLY_DIRECTION_KEY, Direction.class)
+                    : Direction.NORTH;
+
             wheelAngle = -wheelAngle;
             boolean inInstancedContraption = vb == null;
 
             BogeyModelData frame = getTransform(DF11G_FRAME, ms, inInstancedContraption);
-            frame.rotateY(180).translate(0,0.375,0).render(ms, light, vb);
-
             BogeyModelData[] wheels = getTransform(DF11G_WHEEL, ms, inInstancedContraption, 3);
 
-            for (int side = -1; side < 2; side++) {
-                if (!inInstancedContraption)
-                    ms.pushPose();
-                BogeyModelData wheel = wheels[side + 1];
-                wheel.translate(0, 0.88, ((double)side) * 2d)
-                        .rotateX(-wheelAngle)
-                        .render(ms, light, vb);
-                if (!inInstancedContraption)
-                    ms.popPose();
+            if (direction == Direction.NORTH || direction == Direction.WEST){
+                if (forwards){
+                    frame.rotateY(180).translate(0,0.375,0).render(ms, light, vb);
+
+                    for (int side = -1; side < 2; side++) {
+                        if (!inInstancedContraption)
+                            ms.pushPose();
+                        BogeyModelData wheel = wheels[side + 1];
+                        wheel.translate(0, 0.88, ((double)side) * 2d)
+                                .rotateX(-wheelAngle)
+                                .render(ms, light, vb);
+                        if (!inInstancedContraption)
+                            ms.popPose();
+                    }
+                }else {
+                    frame.translate(0,0.375,0).render(ms, light, vb);
+
+                    for (int side = -1; side < 2; side++) {
+                        if (!inInstancedContraption)
+                            ms.pushPose();
+                        BogeyModelData wheel = wheels[side + 1];
+                        wheel.translate(0, 0.88, ((double)side) * 2d)
+                                .rotateX(wheelAngle)
+                                .render(ms, light, vb);
+                        if (!inInstancedContraption)
+                            ms.popPose();
+                    }
+                }
+            }else {
+                frame.rotateY(180).translate(0,0.375,0).render(ms, light, vb);
+
+                for (int side = -1; side < 2; side++) {
+                    if (!inInstancedContraption)
+                        ms.pushPose();
+                    BogeyModelData wheel = wheels[side + 1];
+                    wheel.translate(0, 0.88, ((double)side) * 2d)
+                            .rotateX(-wheelAngle)
+                            .render(ms, light, vb);
+                    if (!inInstancedContraption)
+                        ms.popPose();
+                }
             }
         }
 
@@ -93,5 +176,28 @@ public class DF11GBogeyRenderer {
             this.createModelInstance(materialManager, DF11G_FRAME);
             this.createModelInstance(materialManager, DF11G_WHEEL, 3);
         }
+    }
+
+    protected static boolean isForwards(CompoundTag bogeyData, boolean inContraption) {
+        boolean isForwards = bogeyData.contains(Constants.BOGEY_DIRECTION_KEY) && bogeyData.getBoolean(Constants.BOGEY_DIRECTION_KEY);
+
+        Direction direction = bogeyData.contains(Constants.BOGEY_ASSEMBLY_DIRECTION_KEY)
+                ? NBTHelper.readEnum(bogeyData, Constants.BOGEY_ASSEMBLY_DIRECTION_KEY, Direction.class)
+                : Direction.NORTH;
+
+        boolean isLinked = true;
+        if (bogeyData.contains(Constants.BOGEY_LINK_KEY))
+            isLinked = bogeyData.getBoolean(Constants.BOGEY_LINK_KEY);
+
+        boolean isPosotive = isDirectionPosotive(direction);
+
+        if (isLinked && inContraption && isPosotive)
+            isForwards = !isForwards;
+
+        return isPosotive == isForwards;
+    }
+
+    public static boolean isDirectionPosotive(Direction direction) {
+        return switch (direction) { case NORTH, WEST, UP -> true; case SOUTH, DOWN, EAST -> false; };
     }
 }
--- src/main/java/willow/train/kuayue/mixin/MixinStationBlockEntity.java
+++ src/main/java/willow/train/kuayue/mixin/MixinStationBlockEntity.java
@@ -1,36 +1,37 @@
 package willow.train.kuayue.mixin;
 
 import com.simibubi.create.AllBlocks;
-import com.simibubi.create.AllBogeyStyles;
+import com.simibubi.create.content.trains.bogey.AbstractBogeyBlock;
 import com.simibubi.create.content.trains.bogey.AbstractBogeyBlockEntity;
-import com.simibubi.create.content.trains.bogey.BogeySizes;
-import com.simibubi.create.content.trains.bogey.BogeyStyle;
 import com.simibubi.create.content.trains.station.GlobalStation;
 import com.simibubi.create.content.trains.station.StationBlockEntity;
 import com.simibubi.create.content.trains.track.ITrackBlock;
-import com.simibubi.create.foundation.blockEntity.SmartBlockEntity;
-import com.simibubi.create.foundation.utility.Pair;
+import com.simibubi.create.foundation.utility.NBTHelper;
 import com.tterrag.registrate.util.entry.BlockEntry;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.levelgen.structure.BoundingBox;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
-import willow.train.kuayue.init.KYTrackMaterials;
+import willow.train.kuayue.base.Constants;
 
 import javax.annotation.Nullable;
 
 @Mixin(value = StationBlockEntity.class, remap = false)
-public abstract class MixinStationBlockEntity extends SmartBlockEntity {
+public abstract class MixinStationBlockEntity extends BlockEntity {
+
+    @Shadow
+    Direction assemblyDirection;
 
     @Shadow
     @Nullable
@@ -80,5 +81,35 @@ public abstract class MixinStationBlockEntity extends SmartBlockEntity {
             bogeyBE.setBogeyStyle(style);
         }
     }*/
+
+    @Inject(
+            method = "trackClicked",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/world/level/Level;setBlock(Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;I)Z",
+                    shift = At.Shift.AFTER
+            )
+    )
+    public void addData(Player player, InteractionHand hand, ITrackBlock track, BlockState state, BlockPos pos, CallbackInfoReturnable<Boolean> cir) {
+        if (level == null)
+            return;
+
+        BlockPos up = new BlockPos(track.getUpNormal(level, pos, state));
+        BlockPos down = new BlockPos(track.getUpNormal(level, pos, state).scale(-1));
+
+        boolean upsideDown = (player.getViewXRot(1.0F) < 0 && (track.getBogeyAnchor(level, pos, state)).getBlock() instanceof AbstractBogeyBlock<?> bogey && bogey.canBeUpsideDown());
+        BlockPos targetPos = upsideDown ? pos.offset(down) : pos.offset(up);
+
+        BlockEntity blockEntity = level.getBlockEntity(targetPos);
+
+        if (!(blockEntity instanceof AbstractBogeyBlockEntity bogeyBlockEntity))
+            return;
+
+        CompoundTag bogeyData = bogeyBlockEntity.getBogeyData();
+
+        NBTHelper.writeEnum(bogeyData, Constants.BOGEY_ASSEMBLY_DIRECTION_KEY, assemblyDirection);
+
+        bogeyBlockEntity.setBogeyData(bogeyData);
+    }
 }
 
